f158c49c67e5e5e152e448dd82d568174b7073b5
itzneily <neelimaasokligi@gmail.com>
2025-03-06 07:25:48 +0530

    bg_lib_image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,31 @@
 		<h1 class="logo">Welcome to the Library</h1>
 		<p>
 			<a href="/login">Login</a> to access your account and personalized recommendations.
+			<br><br>
 		</p>
 		{/if}
-		<div class="right"></div>
-		<div class="left">
+		<div class="search-container">
 			<form action="/catalog" method="get">
 				<input type="text" name="q" placeholder="Search for books..." required />
 				<button type="submit">Search</button>
 			</form>
 		</div>
+		<br>
+		<div class="image-container">
+			<img src="library image.png" alt="Library Bookshelf">
+		</div>
+		<div>
+			<p>Explore books and get personalized recommendations.</p>
+		</div>
+				
   </main>
   <style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: 'Poppins', sans-serif;
+	}
 	main {
 	  background-image: url('loginpage_bg.png');
 	  background-size: cover;
@@ -30,7 +44,7 @@
 	  justify-content: center;
 	  align-items: center;
 	  text-align: center;
-	  color: white; /* Ensures text is readable */
+	  color: white;
 	}
 
 	.logo {
@@ -60,23 +74,23 @@
 		color: #666;
 		font-style: italic;
 	}
-
+	
 	
 	input[type="text"]:focus {
 		background-color: #f3e6c9; 
 		box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
 	}
 	button {
-    background-color: #EDDDBF; 
-	width: auto;
-    color: #333; 
-    font-size: 16px;
-    font-weight: bold;
-    border: none;
-    border-radius: 40px;
-    padding: 10px 20px;
-    cursor: pointer;
-    transition: background-color 0.3s, transform 0.2s;
+		background-color: #EDDDBF; 
+		width: auto;
+		color: #333; 
+		font-size: 16px;
+		font-weight: bold;
+		border: none;
+		border-radius: 40px;
+		padding: 10px 20px;
+		cursor: pointer;
+		transition: background-color 0.3s, transform 0.2s;
 	}
 	button:hover {
 		background-color: #d4c1a1;
@@ -85,5 +99,22 @@
 	button:active {
 		background-color: #b8a389;
 		transform: scale(0.98);
+	}
+
+	.image-container {
+		position: relative;
+		width: 100%;
+		max-width: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0 20px;
+		overflow: hidden;
+	}
+
+	.image-container img {
+		width: 80%;
+		height: auto;
+		border-radius: 150px;
 	}
   </style>
